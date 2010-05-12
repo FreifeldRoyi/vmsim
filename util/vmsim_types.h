7c50798a0882beb9f92fcf1593ae96f7ff33f2f4
tom <tom@localhost>
2010-05-12 16:33:55 +0300

    Add some needed types

--- util/vmsim_types.h
+++ util/vmsim_types.h
@@ -1,9 +1,34 @@
 #ifndef VMSIM_TYPES_H_
 #define VMSIM_TYPES_H_
 
-typedef int BOOL;
-
 #define TRUE 1
 #define FALSE 0
+typedef int BOOL;
+
+typedef unsigned char BYTE;
+
+typedef enum {ecSuccess, ecFail, ecNotFound} errcode_t;
+
+typedef unsigned procid_t;
+
+typedef struct
+{
+	procid_t pid;
+	unsigned page;
+}virt_addr_t; //virtual(logical) address
+
+typedef struct
+{
+	unsigned page;
+	unsigned offset;
+}disk_addr_t; //disk address;
+
+typedef unsigned phys_addr_t; //physical address
+
+typedef struct
+{
+	disk_addr_t disk_block_start;
+	unsigned disk_block_npages;
+}process_t;
 
 #endif /* VMSIM_TYPES_H_ */
