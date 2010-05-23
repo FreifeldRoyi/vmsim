f5754a4a3cc9cdfe988823ba494404538019c098
tom <tom@localhost>
2010-05-23 22:48:29 +0300

    Complete vararg logger API for INFO and ERROR

--- util/logger.h
+++ util/logger.h
@@ -14,7 +14,15 @@ typedef enum {lvDebug, lvInfo, lvError} log_level_t;
 #define DEBUG3(_msg, _p1, _p2, _p3) LOG3(lvDebug, _msg, _p1, _p2, _p3)
 #define DEBUG4(_msg, _p1, _p2, _p3, _p4) LOG4(lvDebug, _msg, _p1, _p2, _p3, _p4)
 #define INFO(_msg) LOG(lvInfo, _msg)
+#define INFO1(_msg, _p1) LOG1(lvInfo, _msg, _p1)
+#define INFO2(_msg, _p1, _p2) LOG2(lvInfo, _msg, _p1, _p2)
+#define INFO3(_msg, _p1, _p2, _p3) LOG3(lvInfo, _msg, _p1, _p2, _p3)
+#define INFO4(_msg, _p1, _p2, _p3, _p4) LOG4(lvInfo, _msg, _p1, _p2, _p3, _p4)
 #define ERROR(_msg) LOG(lvError, _msg)
+#define ERROR1(_msg, _p1) LOG1(lvError, _msg, _p1)
+#define ERROR2(_msg, _p1, _p2) LOG2(lvError, _msg, _p1, _p2)
+#define ERROR3(_msg, _p1, _p2, _p3) LOG3(lvError, _msg, _p1, _p2, _p3)
+#define ERROR4(_msg, _p1, _p2, _p3, _p4) LOG4(lvError, _msg, _p1, _p2, _p3, _p4)
 
 void log_write(	const char* file, int line, const char* func,
 				log_level_t level, const char* message,...);
