60d2170a1628d634ffdf2c53195876e7229e6e28
freifeldroyi <freifeldroyi@localhost>
2010-05-28 16:37:35 +0300

    __

--- sim/mm.c
+++ sim/mm.c
@@ -6,11 +6,14 @@
  */
 
 #include "mm.h"
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define READ_START(_mm) rwlock_acuire_read(MM(_mm) -> lock)
-#define WRITE_START(_mm) rwlock_acquire_write(MM(_mm) -> lock)
-#define READ_END(_mm) rwlock_release_read(&(MM(_mm) -> lock)
-#define WRITE_END(_mm) rwlock_release_write(MM(_mm) -> lock)
+#define READ_START(_mm) rwlock_acquire_read(&MM_LOCK(mm))
+#define WRITE_START(_mm) rwlock_acquire_write(&MM_LOCK(mm))
+#define READ_END(_mm) rwlock_release_read(&MM_LOCK(mm))
+#define WRITE_END(_mm) rwlock_release_write(&MM_LOCK(mm))
 
 static BOOL is_page_allocated(mm_t* mm, int page)
 {
@@ -36,6 +39,8 @@ errcode_t mm_init(mm_t* mm, int npages, int pagesize)
 
 	bitmap_init(&MM_BITMAP(mm), npages);
 	rwlock_init(&MM_LOCK(mm));
+
+	return TRUE;
 }
 
 errcode_t mm_get_page(mm_t* mm, int page, BYTE* page_data)
@@ -51,7 +56,7 @@ errcode_t mm_get_page(mm_t* mm, int page, BYTE* page_data)
 		return ecFail;
 	}
 
-	memcpy(page_data, get_page_start(mm, page), page_data, MM_PAGE_SIZE(mm));
+	memcpy(page_data, get_page_start(mm, page), MM_PAGE_SIZE(mm));
 	READ_END(mm);
 	return ecSuccess;
 }
