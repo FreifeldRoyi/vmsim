ea2151cf2f0336062ffece1a519949fe60015f95
freifeld royi <freifeld royi@localhost>
2010-06-02 02:51:53 +0300

    -_- merge problems

--- main.c
+++ main.c
@@ -18,34 +18,3 @@ int main()
 	cunit_destroy();
 	return 0;
 }
-/*
-static char* new_alloc_strcat(char* dest, char* src)
-{
-	char* to_return = calloc(0,sizeof(dest) + sizeof(src) + 1);
-
-	int i = 0, dest_len = strlen(dest), src_len = strlen(src);
-	for (i = 0; i < dest_len; ++i)
-	{
-		to_return[i] = dest[i];
-	}
-	for (i = 0; i < src_len; ++i)
-	{
-		to_return[i + dest_len] = src[i];
-	}
-	to_return[dest_len + src_len + 1] = '\0';
-
-	return to_return;
-}
-
-int main()
-{
-	//print_prompt();
-	char* s = "hello ";
-	char* s2 = "world\n";
-	char* s3 = new_alloc_strcat(s,s2);
-	printf("%s",s3);
-
-	return 0;
-}*/
-
-
