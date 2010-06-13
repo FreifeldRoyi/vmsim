773d28b7548794b571cef1a23739b8e67e89b517
freifeld royi <freifeld royi@localhost>
2010-06-13 15:39:49 +0300

    Document header files

--- vmsim/ui_app.h
+++ vmsim/ui_app.h
@@ -31,10 +31,31 @@ typedef struct _ui_cmd_t
  */
 ui_cmd_t get_command();
 
+/**
+ * Starts a creation chain, which in the end a new process should be created and run.
+ * If not a proper message will be printed
+ *
+ * @param cmd - the command
+ * @param app_data -the app data
+ *
+ * @return TRUE if process is created FALSE otherwise
+ */
 BOOL do_create_process(ui_cmd_t* cmd, app_data_t* app_data);
 
+/**
+ * Sends a delete message to a process specified
+ * UI waits untill deleting is done
+ *
+ * @param -the command
+ * @param app_data - the app data
+ */
 BOOL do_del_process(ui_cmd_t* cmd, app_data_t* app_data);
 
+/**
+ * The next functions sends a message to a process' mail box
+ * according to @param cmd params
+ * UI will not wait until operation has finished
+ */
 BOOL do_read(ui_cmd_t* cmd, app_data_t* app_data);
 
 BOOL do_loop_read(ui_cmd_t* cmd, app_data_t* app_data);
@@ -47,25 +68,32 @@ BOOL do_write(ui_cmd_t* cmd, app_data_t* app_data);
 
 BOOL do_loop_write(ui_cmd_t* cmd, app_data_t* app_data);
 
+/**
+ * The next function will cause a chain of function calling, causing
+ * a corresponding printing in the end
+ */
 BOOL do_hit_rate(ui_cmd_t* cmd, app_data_t* app_data);
 
 BOOL do_print_MM(ui_cmd_t* cmd, app_data_t* app_data);
 
-/**
- *	prints the IPT
- */
 BOOL do_print_MMU_table(ui_cmd_t* cmd, app_data_t* app_data);
 
 BOOL do_print_registers(ui_cmd_t* cmd, app_data_t* app_data);
 
 BOOL do_print_HAT(ui_cmd_t* cmd, app_data_t* app_data);
 
+/**
+ * change logger mode
+ */
 BOOL do_monitor(ui_cmd_t* cmd, app_data_t* app_data);
 
 BOOL do_no_monitor(ui_cmd_t* cmd, app_data_t* app_data);
 
 BOOL do_debug_mode(ui_cmd_t* cmd, app_data_t* app_data);
 
+/**
+ * read a batch file according to @param cmd params
+ */
 BOOL do_batch_file(ui_cmd_t* cmd, app_data_t* app_data);
 
 /**
